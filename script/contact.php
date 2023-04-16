c8db74457f78e15aa0eb833c7cb2492f23ecc0cf
Lucas Spitzer <104842840+pangolino30@users.noreply.github.com>
2023-04-16 13:34:39 +0200

    Ajout redirection depuis formulaire de contact

--- script/contact.php
+++ script/contact.php
@@ -3,5 +3,6 @@ $nom = $_POST['nom'];
 $email = $_POST['email'];
 $sujet = $_POST['sujet'];
 $message = $_POST['message'];
-mail("spitzerl@proton.me", "$sujet de $nom", $message, "Reply-To: $email")
+mail("spitzerl@proton.me", "$sujet de $nom", $message, "Reply-To: $email");
+header("Location: ../index.html");
 ?>
