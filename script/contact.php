8d3224c2772afcf2cf0d8b6ca9630d0f5ac82df5
Lucas Spitzer <104842840+pangolino30@users.noreply.github.com>
2023-04-16 13:40:20 +0200

    Changement du nom de l'email

--- script/contact.php
+++ script/contact.php
@@ -3,7 +3,7 @@ $nom = $_POST['nom'];
 $email = $_POST['email'];
 $sujet = $_POST['sujet'];
 $message = $_POST['message'];
-mail("spitzerl@proton.me", "$sujet de $nom", $message, "Reply-To: $email");
+mail("spitzerl@proton.me", "[CONTACT SITE] $sujet de $nom", $message, "Reply-To: $email");
 header("Location: ../index.html");
 exit();
 ?>
